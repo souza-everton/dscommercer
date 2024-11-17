6e3eed3ff6d67a5f761a4a4ab14dfe555b129bc9
Everton Souza <eve.souzalemos@gmail.com>
2024-11-17 17:51:46 -0300

    Project Created

---
+++ src/main/java/com/devsuperior/dscommercer/DsCommercerDevSuperiorApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscommercer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsCommercerDevSuperiorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsCommercerDevSuperiorApplication.class, args);
+	}
+
+}
