5bcfe050779cc34ec0d3212d53f3af75af1f5944
Everton Souza <eve.souzalemos@gmail.com>
2024-11-17 22:08:22 -0300

    OrderItem, OrderItemPK

--- src/main/java/com/devsuperior/dscommercer/entities/User.java
+++ src/main/java/com/devsuperior/dscommercer/entities/User.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,6 +21,8 @@ public class User {
 	private Long id;
 	
 	private String nome;
+	
+	@Column(unique = true)
 	private String email;
 	private String phone;
 	private LocalDate birthDate;
